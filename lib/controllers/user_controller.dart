0b0f2c0ed4b3e161c7e613020fcbd6169e1221c8
Marco Antonio <marco.ag.pegoraro@hotmail.com>
2020-03-25 20:06:16 -0300

    Só com controller.

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -1,12 +1,6 @@
 import 'package:gdg_demo/gdg_demo.dart';
 
 class UserController extends ResourceController {
-  // UserController(this.context) {
-  //   acceptedContentTypes = [ContentType.json];
-  // }
-
-  // ManagedContext context;
-
   @Operation.get()
   Future<Response> getUsers() async {
     return Response.ok("teste");
