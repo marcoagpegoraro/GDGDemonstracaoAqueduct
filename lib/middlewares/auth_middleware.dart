7b914dbdd0933af8f2491720223a0dcd33c746fe
Marco Antonio <marco.ag.pegoraro@hotmail.com>
2020-03-26 16:52:22 -0300

    comentarios na atuh

--- lib/middlewares/auth_middleware.dart
+++ lib/middlewares/auth_middleware.dart
@@ -4,11 +4,16 @@ import 'dart:convert';
 class AuthMiddleware extends Controller {
   @override
   FutureOr<RequestOrResponse> handle(Request request) {
+    //Buscando informações do header
     final token = request.raw.headers["authorization"][0];
-    print(token);
+
+    //Pegando somente a parte criptografada do header
     final tokenBase64 = token.split(" ")[1];
 
+    //decriptando o token
     final decryptToken = utf8.fuse(base64).decode(tokenBase64);
+
+    //pegando os valores de username e senha
     final username = decryptToken.split(":")[0];
     final password = decryptToken.split(":")[1];
 
